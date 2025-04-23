982127817c35d0a10f6856bd2a45ba3281f4354d
Shruthi Chella <chshruthi07@gmail.com>
2025-04-23 17:35:13 -0500

    javascript and css files were seperated

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-console.log("To-Do App Started, bro!,");
-console.log(" Shruthi To-Do App Started, bro!,");
-console.log(" Shruthi To-Do App Started, 1.0 bro!,");
+function addTask() {
+    let taskInput = document.getElementById("task");
+    let taskValue = taskInput.value.trim();
+    if (taskValue === "") return;
+    
+    let li = document.createElement("li");
+    li.innerHTML = taskValue + ' <button onclick="this.parentElement.remove()">X</button>';
+    document.getElementById("taskList").appendChild(li);
+    
+    taskInput.value = "";
+}
+
+// console.log("To-Do App Started, bro!,");
+// console.log(" Shruthi To-Do App Started, bro!,");
+// console.log(" Shruthi To-Do App Started, 1.0 bro!,");
